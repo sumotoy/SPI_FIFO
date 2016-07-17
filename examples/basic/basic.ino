7ede0f4a57027851387939aa54b63ae3be8c4f41
sumotoy <sumotoy@gmail.com>
2016-07-17 18:36:20 +0200

    added a simple example

---
+++ examples/basic/basic.ino
@@ -0,0 +1,56 @@
+#include <SPI.h>
+#include <SPI_FIFO_t3.h>
+
+
+uint8_t errorCode = 0;
+
+SPI_FIFO_t3 mySPI = SPI_FIFO_t3(10,9);
+
+
+void setup()
+{
+	Serial.begin(38400);
+	long unsigned debug_start = millis();
+	while (!Serial && ((millis() - debug_start) <= 5000));
+	errorCode = mySPI.begin();
+	if (errorCode) {
+		Serial.println(">> SPI FIFO ready <<");
+		Serial.print("Using bus:SPI");
+		Serial.println(mySPI.getSPIbus());
+	}
+	else {
+		Serial.println(">> ERROR <<");
+		if (bitRead(errorCode, 0) == 0) {
+			Serial.print("mosi,sclk or miso pin mismatch!");
+		}
+		else if (bitRead(errorCode, 1) == 0){
+			Serial.print("cs pin mismatch");
+		}
+		else if (bitRead(errorCode, 2) == 0) {
+			Serial.print("dc pin mismatch");
+		}
+	}
+
+}
+
+void loop()
+{
+
+
+}
+
+/*
+Here's an example of use, since we are using FIFO and the plan is use CS pin only when needed,
+we have 2 basic type of functions, _cont and _last, _cont pull down CS, _last pull Up it (disable)
+so _last must be used as last one, if you just have one trasfer, use always _last.
+It's important start with startTransaction and end with endTransaction!
+In this example a byte is tranfered using only CS (even you you are using the dc pin as well)
+If you want transfer a byte in command mode (such used in some OLED/TFT libraries), use:
+mySPI.writeByte_last(data,true);
+*/
+void writeByte(byte data)
+{
+	mySPI.startTransaction();
+	mySPI.writeByte_last(data);
+	mySPI.endTransaction();
+}
